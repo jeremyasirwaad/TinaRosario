2898302e384e0b4d4fa5581f38bd30de205fff46
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-10-06 10:20:29 +0530

    contactus added

---
+++ src/Components/Contactus/Contactus.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Footer } from "../Footer/Footer";
+import "./contact.css";
+
+export const Contactus = () => {
+	return (
+		<div>
+			<div className="contactbanner">
+				<div className="contactbannerinner">
+					<span>Contact Us</span>
+					<span>To design your style</span>
+				</div>
+			</div>
+			<Footer></Footer>
+		</div>
+	);
+};
