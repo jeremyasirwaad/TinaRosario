c936672b934f17e90db05a5f372d94197fc4c583
raghav <raghavjindal0212@gmail.com>
2022-10-08 13:57:27 +0530

    added simple carousel

--- src/Components/Landing/Caro.js
+++ src/Components/Landing/Caro.js
@@ -2,62 +2,36 @@ import React from "react";
 import img1 from "./l1-min.jpg";
 import img4 from "./l4-min.jpg";
 import img5 from "./l5-min.jpg";
+import caro from "./caro.svg";
 export const Caro = () => {
   return (
-    <div style={{ backgroundColor: "teal" }} id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active" style={{}}>
-          <div className="limginfo">
-            {/* <span className="limginfo1">Lorem ipsum, dolor. Quo odio t</span> */}
-            <span className="limginfo2">Customization as per your style, desire and comfort.</span>
-
-            <button className="limginfo3">Buy Now</button>
+    <div style={{ height: "450px", backgroundColor: "black" }} class="carousel slide" data-ride="carousel">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div style={{ backgroundSize: "contain" }} class="carousel-item active">
+            <div style={{ backgroundColor: "black", height: "450px" }}></div>
           </div>
-          <img class="d-block w-100" src={img5} alt="First slide" style={{ height: "700px" }} />
-        </div>
-        <div class="carousel-item">
-          <div className="limginfo">
-            {/* <span className="limginfo1">Lorem ipsum, dolor. Quo odio t</span> */}
-            <span className="limginfo2">Usage of premium quality fabrics</span>
-
-            <button className="limginfo3">Buy Now</button>
+          <div class="carousel-item">
+            <div style={{ backgroundColor: "#4C2A76", height: "450px" }}></div>
           </div>
-          <img class="d-block w-100" src={img4} alt="Second slide" style={{ height: "700px" }} />
-        </div>
-        <div class="carousel-item ">
-          <div className="limginfoL">
-            {/* <span className="limginfo1">Lorem ipsum, dolor. Quo odio t</span> */}
-            <span className="limginfo2">
-              One Stop platform for all your needs from basic handlooms to wedding grandeurs
-            </span>
-            <button className="limginfo3">Buy Now</button>
+          <div class="carousel-item">
+            <div style={{ backgroundColor: "#270641", height: "450px" }}></div>
           </div>
-          <img class="d-block w-100" src={img1} alt="Third slide" style={{ height: "700px" }} />
         </div>
-        <div class="carousel-item">
-          <div className="limginfo">
-            {/* <span className="limginfo1">Lorem ipsum, dolor. Quo odio t</span> */}
-            <span className="limginfo2">No sitting back until we find the smile of contentment in you.</span>
-
-            <button className="limginfo3">Buy Now</button>
-          </div>
-          <img class="d-block w-100" src={img4} alt="Second slide" style={{ height: "700px" }} />
-        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
   );
 };
