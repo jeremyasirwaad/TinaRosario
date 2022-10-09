53673f39d8726f1ab093bd0aed0e76f4cfd8b517
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-10-09 13:36:16 +0530

    fixed changed in img consult

--- src/Components/Pages/ImageCons/ImageCons.js
+++ src/Components/Pages/ImageCons/ImageCons.js
@@ -17,105 +17,96 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 const ImageCons = () => {
-  const [value, setValue] = React.useState(dayjs());
+	const [value, setValue] = React.useState(dayjs());
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
-  };
-  return (
-    <div className="imagecons">
-      <div className="topbar">
-        <div className="background">
-          <span className="wb1">Image Consulting</span>
-          <span className="wb2">"Pride in the new look"</span>
-        </div>
-      </div>
-      <div className="weddingbg">
-        <div className="icons">
-          <div className="icon-holder">
-            <img src={img1} />
-            <p>Confused what looks good on you?</p>
-          </div>
-          <div className="icon-holder">
-            <img src={img2} />
-            <p>Unsure which dress suits your personality?</p>
-          </div>
-          <div className="icon-holder">
-            <img src={img3} />
-            <p>Trouble identifying the right color choices?</p>
-          </div>
-        </div>
-        <div className="img-row">
-          <img src={back} />
-          <p>
-            While we hold In-depth conversations to understand you better.
-            <br /> It is important we Fashion is mostly an individual choice. Uncover your aspirations in THE DESIGN
-            HOUSE.
-          </p>
-        </div>
+	const handleChange = (newValue) => {
+		setValue(newValue);
+	};
+	return (
+		<div className="imagecons">
+			<div className="topbar">
+				<div className="background">
+					<span className="wb1">Image Consulting</span>
+					<span className="wb2">"Pride in the new look"</span>
+				</div>
+			</div>
+			<div className="weddingbg">
+				<div className="icons">
+					<div className="icon-holder">
+						<img src={img1} />
+						<span>Confused what looks good on you?</span>
+					</div>
+					<div className="icon-holder">
+						<img src={img2} />
+						<span>Unsure which dress suits your personality?</span>
+					</div>
+					<div className="icon-holder">
+						<img src={img3} />
+						<span>Trouble identifying the right color choices?</span>
+					</div>
+				</div>
+				<div className="img-row">
+					<img src={back} />
+					<p>
+						While we hold In-depth conversations to understand you better.
+						<br /> It is important we Fashion is mostly an individual choice.
+						Uncover your aspirations in THE DESIGN HOUSE.
+					</p>
+				</div>
 
-        <div className="steps">
-          <img src={steps} />
-        </div>
-        <div className="steps">
-          <img src={smilebar} />
-        </div>
-        <div style={{}} className="weddingforms imageform">
-          <span className="weddingt2">To discuss further, provide your </span>
-          <TextField
-            className="image-textfield"
-            id="outlined-basic"
-            label="Name"
-            variant="outlined"
-            style={{ width: "500px", marginTop: "30px" }}
-          />
-          <TextField
-            className="image-textfield"
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            style={{ width: "500px", marginTop: "30px" }}
-          />
-          {/* <LocalizationProvider dateAdapter={AdapterDayjs} className="datewidth">
-            <Stack spacing={3} className="datewidth">
-              <MobileDatePicker
-                label="When is the big day ?"
-                inputFormat="MM/DD/YYYY"
-                value={value}
-                onChange={handleChange}
-                renderInput={(params) => <TextField {...params} />}
-                className="datewidth"
-                disablePast
-              />
-            </Stack>
-          </LocalizationProvider> */}
-          <TextField
-            className="image-textfield datewidth"
-            id="outlined-basic"
-            label="Any special note	"
-            variant="outlined"
-            style={{ marginTop: "30px" }}
-          />
-          <LocalizationProvider dateAdapter={AdapterDayjs} className="datewidth">
-            <Stack spacing={3} className="datewidth">
-              <MobileDatePicker
-                className="image-textfield datewidth"
-                label="Appointment Date"
-                inputFormat="MM/DD/YYYY"
-                value={value}
-                onChange={handleChange}
-                renderInput={(params) => <TextField {...params} />}
-                disablePast
-              />
-            </Stack>
-          </LocalizationProvider>
-          <button style={{ marginBottom: "20px" }} className="weddingbookbtn">
-            Book
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="steps">
+					<img src={steps} />
+				</div>
+				<div className="steps">
+					<img src={smilebar} />
+				</div>
+				<div style={{}} className="weddingforms imageform">
+					<span className="weddingt2">To discuss further, provide your </span>
+					<TextField
+						className="image-textfield"
+						id="outlined-basic"
+						label="Name"
+						variant="outlined"
+						style={{ width: "500px", marginTop: "30px" }}
+					/>
+					<TextField
+						className="image-textfield"
+						id="outlined-basic"
+						label="Email"
+						variant="outlined"
+						style={{ width: "500px", marginTop: "30px" }}
+					/>
+					<TextField
+						className="image-textfield datewidth"
+						id="outlined-basic"
+						label="Any special note	"
+						variant="outlined"
+						style={{ marginTop: "30px" }}
+					/>
+
+					<LocalizationProvider
+						dateAdapter={AdapterDayjs}
+						className="datewidth"
+					>
+						<Stack spacing={3} className="datewidth">
+							<MobileDatePicker
+								className="image-textfield datewidth"
+								label="Appointment Date"
+								inputFormat="MM/DD/YYYY"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+								disablePast
+							/>
+						</Stack>
+					</LocalizationProvider>
+					<button style={{ marginBottom: "50px" }} className="weddingbookbtn">
+						Book
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ImageCons;
