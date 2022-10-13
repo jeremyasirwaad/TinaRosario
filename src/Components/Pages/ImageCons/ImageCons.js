78bf9da2d1aa129f576c6895bb96c74ec3afdd6c
raghav <raghavjindal0212@gmail.com>
2022-10-14 02:18:12 +0530

    all forms inegrated!!!!!!

--- src/Components/Pages/ImageCons/ImageCons.js
+++ src/Components/Pages/ImageCons/ImageCons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import back from "./back.svg";
 import "./ImageCons.css";
 import img1 from "./img1.svg";
@@ -15,98 +15,113 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
-
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 const ImageCons = () => {
-	const [value, setValue] = React.useState(dayjs());
+  const url = "http://localhost:3001";
 
-	const handleChange = (newValue) => {
-		setValue(newValue);
-	};
-	return (
-		<div className="imagecons">
-			<div className="topbar">
-				<div className="background">
-					<span className="wb1">Image Consulting</span>
-					<span className="wb2">"Pride in the new look"</span>
-				</div>
-			</div>
-			<div className="weddingbg">
-				<div className="icons">
-					<div className="icon-holder">
-						<img src={img1} />
-						<span>Confused what looks good on you?</span>
-					</div>
-					<div className="icon-holder">
-						<img src={img2} />
-						<span>Unsure which dress suits your personality?</span>
-					</div>
-					<div className="icon-holder">
-						<img src={img3} />
-						<span>Trouble identifying the right color choices?</span>
-					</div>
-				</div>
-				<div className="img-row">
-					<img src={back} />
-					<p>
-						While we hold In-depth conversations to understand you better.
-						<br /> It is important we Fashion is mostly an individual choice.
-						Uncover your aspirations in THE DESIGN HOUSE.
-					</p>
-				</div>
+  const [appointDate, setappointDate] = React.useState(dayjs());
 
-				<div className="steps">
-					<img src={steps} />
-				</div>
-				<div className="steps">
-					<img src={smilebar} />
-				</div>
-				<div style={{}} className="weddingforms imageform">
-					<span className="weddingt2">To discuss further, provide your </span>
-					<TextField
-						className="image-textfield"
-						id="outlined-basic"
-						label="Name"
-						variant="outlined"
-						style={{ width: "500px", marginTop: "30px" }}
-					/>
-					<TextField
-						className="image-textfield"
-						id="outlined-basic"
-						label="Email"
-						variant="outlined"
-						style={{ width: "500px", marginTop: "30px" }}
-					/>
-					<TextField
-						className="image-textfield datewidth"
-						id="outlined-basic"
-						label="Any special note	"
-						variant="outlined"
-						style={{ marginTop: "30px" }}
-					/>
+  const selectappoint = (newLocale) => {
+    setappointDate(newLocale);
+    setformData({ ...formData, appointDate: dayjs(newLocale).format("DD/MM/YYYY") });
+  };
 
-					<LocalizationProvider
-						dateAdapter={AdapterDayjs}
-						className="datewidth"
-					>
-						<Stack spacing={3} className="datewidth">
-							<MobileDatePicker
-								className="image-textfield datewidth"
-								label="Appointment Date"
-								inputFormat="MM/DD/YYYY"
-								value={value}
-								onChange={handleChange}
-								renderInput={(params) => <TextField {...params} />}
-								disablePast
-							/>
-						</Stack>
-					</LocalizationProvider>
-					<button style={{ marginBottom: "50px" }} className="weddingbookbtn">
-						Book
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const [formData, setformData] = useState({ name: "", email: "", note: "", appointDate: "" });
+
+  const handleFormSubmit = async () => {
+    const data = await axios.post(`${url}/formData`, { category: "image consulting", ...formData });
+    if (data) {
+      toast.success("Thank you! Your response if saved with us!");
+      console.log(data);
+    }
+  };
+  return (
+    <div className="imagecons">
+      <Toaster />
+      <div className="topbar">
+        <div className="background">
+          <span className="wb1">Image Consulting</span>
+          <span className="wb2">"Pride in the new look"</span>
+        </div>
+      </div>
+      <div className="weddingbg">
+        <div className="icons">
+          <div className="icon-holder">
+            <img src={img1} />
+            <span>Confused what looks good on you?</span>
+          </div>
+          <div className="icon-holder">
+            <img src={img2} />
+            <span>Unsure which dress suits your personality?</span>
+          </div>
+          <div className="icon-holder">
+            <img src={img3} />
+            <span>Trouble identifying the right color choices?</span>
+          </div>
+        </div>
+        <div className="img-row">
+          <img src={back} />
+          <p>
+            While we hold In-depth conversations to understand you better.
+            <br /> It is important we Fashion is mostly an individual choice. Uncover your aspirations in THE DESIGN
+            HOUSE.
+          </p>
+        </div>
+
+        <div className="steps">
+          <img src={steps} />
+        </div>
+        <div className="steps">
+          <img src={smilebar} />
+        </div>
+        <div style={{}} className="weddingforms imageform">
+          <span className="weddingt2">To discuss further, provide your </span>
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            value={formData.name}
+            onChange={(e) => setformData({ ...formData, name: e.target.value })}
+            variant="outlined"
+            style={{ width: "500px", marginTop: "30px" }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            value={formData.email}
+            onChange={(e) => setformData({ ...formData, email: e.target.value })}
+            variant="outlined"
+            style={{ width: "500px", marginTop: "30px" }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Any special note	"
+            variant="outlined"
+            className="datewidth"
+            value={formData.note}
+            onChange={(e) => setformData({ ...formData, note: e.target.value })}
+            style={{ marginTop: "30px" }}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs} className="datewidth">
+            <Stack spacing={3} className="datewidth">
+              <MobileDatePicker
+                label="Appointment Date"
+                inputFormat="MM/DD/YYYY"
+                value={appointDate}
+                onChange={selectappoint}
+                renderInput={(params) => <TextField {...params} />}
+                className="datewidth"
+                disablePast
+              />
+            </Stack>
+          </LocalizationProvider>
+          <button style={{ cursor: "pointer" }} className="weddingbookbtn" onClick={handleFormSubmit}>
+            Book
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageCons;
--- src/Components/Pages/Wedding.js
+++ src/Components/Pages/Wedding.js
@@ -17,143 +17,160 @@ import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 
 import img1 from "./Group 36 (1).svg";
 import AOS from "aos";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 export const Wedding = () => {
-	const navigate = useNavigate();
+  const url = "http://localhost:3001";
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		AOS.init({
-			duration: 1000,
-			once: true
-		});
-	}, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
 
-	const [value, setValue] = React.useState(dayjs());
+  const [dday, setdday] = React.useState(dayjs());
+  const [appointDate, setappointDate] = React.useState(dayjs());
 
-	const handleChange = (newValue) => {
-		setValue(newValue);
-	};
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center"
-				// backgroundColor: "black"
-			}}
-		>
-			<div className="weddingbanner">
-				<div className="weddingbannerinner">
-					<span className="wb1">Wedding Collections</span>
-					<span className="wb2">
-						As you hold hands and shine together and promise to live as one
-						forever, you need to look your best as you've always dreamt.
-					</span>
-				</div>
-			</div>
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center"
-				}}
-				className="weddingbg"
-			>
-				<div className="wedsec1" style={{ marginTop: "100px" }}>
-					<div className="wedsec1img">
-						<img src={img1} alt="" />
-					</div>
+  const selectdday = (newLocale) => {
+    setdday(newLocale);
+    setformData({ ...formData, dday: dayjs(newLocale).format("DD/MM/YYYY") });
+  };
 
-					<div className="wedsec1cont">
-						<span>
-							We understand wedding chores could overwhelm you, Here we are to
-							take off your burden of how you're going to look.
-						</span>
-						<span style={{ marginTop: "10px" }}>
-							Here we are to take off your burden of how you're going to look.
-						</span>
-					</div>
-				</div>
-				<div className="wedsec2">
-					<div className="wedsec1img">
-						<img src={img1} alt="" />
-					</div>
+  const selectappoint = (newLocale) => {
+    setappointDate(newLocale);
+    setformData({ ...formData, appointDate: dayjs(newLocale).format("DD/MM/YYYY") });
+  };
 
-					<div className="wedsec2cont">
-						<span>Get in touch and allow us to unfold</span>
-						<span>your dream attire</span>
-					</div>
-				</div>
-				<div className="wedsec1" style={{ marginTop: "80px" }}>
-					<div className="wedsec1img">
-						<img src={img1} alt="" />
-					</div>
+  const [formData, setformData] = useState({ name: "", email: "", dday: "", note: "", appointDate: "" });
 
-					<div className="wedsec1cont">
-						<span>
-							A few ounces of personal attention along with some love and care.
-						</span>
-						<span style={{ marginTop: "10px" }}>
-							We assure you a perfect attire that'll be spoken about, forever.
-						</span>
-					</div>
-				</div>
-				<div className="weddingforms">
-					<span className="weddingt2">To discuss further, provide your </span>
-					<TextField
-						id="outlined-basic"
-						label="Name"
-						variant="outlined"
-						style={{ width: "500px", marginTop: "30px" }}
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Email"
-						variant="outlined"
-						style={{ width: "500px", marginTop: "30px" }}
-					/>
-					<LocalizationProvider
-						dateAdapter={AdapterDayjs}
-						className="datewidth"
-					>
-						<Stack spacing={3} className="datewidth">
-							<MobileDatePicker
-								label="When is the big day ?"
-								inputFormat="MM/DD/YYYY"
-								value={value}
-								onChange={handleChange}
-								renderInput={(params) => <TextField {...params} />}
-								className="datewidth"
-								disablePast
-							/>
-						</Stack>
-					</LocalizationProvider>
-					<TextField
-						id="outlined-basic"
-						label="Any special note	"
-						variant="outlined"
-						className="datewidth"
-						style={{ marginTop: "30px" }}
-					/>
-					<LocalizationProvider
-						dateAdapter={AdapterDayjs}
-						className="datewidth"
-					>
-						<Stack spacing={3} className="datewidth">
-							<MobileDatePicker
-								label="Appointment Date"
-								inputFormat="MM/DD/YYYY"
-								value={value}
-								onChange={handleChange}
-								renderInput={(params) => <TextField {...params} />}
-								className="datewidth"
-								disablePast
-							/>
-						</Stack>
-					</LocalizationProvider>
-					<button className="weddingbookbtn">Book</button>
-				</div>
-			</div>
-			
-		</div>
-	);
+  const handleFormSubmit = async () => {
+    const data = await axios.post(`${url}/formData`, { category: "wedding collection", ...formData });
+    if (data) {
+      toast.success("Thank you! Your response if saved with us!");
+      console.log(data);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // backgroundColor: "black"
+      }}
+    >
+      <Toaster />
+      <div className="weddingbanner">
+        <div className="weddingbannerinner">
+          <span className="wb1">Wedding Collections</span>
+          <span className="wb2">
+            As you hold hands and shine together and promise to live as one forever, you need to look your best as
+            you've always dreamt.
+          </span>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        className="weddingbg"
+      >
+        <div className="wedsec1" style={{ marginTop: "100px" }}>
+          <div className="wedsec1img">
+            <img src={img1} alt="" />
+          </div>
+
+          <div className="wedsec1cont">
+            <span>
+              We understand wedding chores could overwhelm you, Here we are to take off your burden of how you're going
+              to look.
+            </span>
+            <span style={{ marginTop: "10px" }}>Here we are to take off your burden of how you're going to look.</span>
+          </div>
+        </div>
+        <div className="wedsec2">
+          <div className="wedsec1img">
+            <img src={img1} alt="" />
+          </div>
+
+          <div className="wedsec2cont">
+            <span>Get in touch and allow us to unfold</span>
+            <span>your dream attire</span>
+          </div>
+        </div>
+        <div className="wedsec1" style={{ marginTop: "80px" }}>
+          <div className="wedsec1img">
+            <img src={img1} alt="" />
+          </div>
+
+          <div className="wedsec1cont">
+            <span>A few ounces of personal attention along with some love and care.</span>
+            <span style={{ marginTop: "10px" }}>We assure you a perfect attire that'll be spoken about, forever.</span>
+          </div>
+        </div>
+        <div className="weddingforms">
+          <span className="weddingt2">To discuss further, provide your </span>
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            value={formData.name}
+            onChange={(e) => setformData({ ...formData, name: e.target.value })}
+            variant="outlined"
+            style={{ width: "500px", marginTop: "30px" }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            value={formData.email}
+            onChange={(e) => setformData({ ...formData, email: e.target.value })}
+            variant="outlined"
+            style={{ width: "500px", marginTop: "30px" }}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs} className="datewidth">
+            <Stack spacing={3} className="datewidth">
+              <MobileDatePicker
+                label="When is the big day ?"
+                inputFormat="MM/DD/YYYY"
+                value={dday}
+                onChange={selectdday}
+                renderInput={(params) => <TextField {...params} />}
+                className="datewidth"
+                disablePast
+              />
+            </Stack>
+          </LocalizationProvider>
+          <TextField
+            id="outlined-basic"
+            label="Any special note	"
+            variant="outlined"
+            className="datewidth"
+            value={formData.note}
+            onChange={(e) => setformData({ ...formData, note: e.target.value })}
+            style={{ marginTop: "30px" }}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs} className="datewidth">
+            <Stack spacing={3} className="datewidth">
+              <MobileDatePicker
+                label="Appointment Date"
+                inputFormat="MM/DD/YYYY"
+                value={appointDate}
+                onChange={selectappoint}
+                renderInput={(params) => <TextField {...params} />}
+                className="datewidth"
+                disablePast
+              />
+            </Stack>
+          </LocalizationProvider>
+          <button style={{ cursor: "pointer" }} className="weddingbookbtn" onClick={handleFormSubmit}>
+            Book
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
