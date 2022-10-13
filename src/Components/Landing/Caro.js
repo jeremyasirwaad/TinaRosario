48f64dee1e92a88d2da77c2688fed8fbb64fd23a
raghav <raghavjindal0212@gmail.com>
2022-10-13 21:56:30 +0530

    carousel and navbar change

--- src/Components/Landing/Caro.js
+++ src/Components/Landing/Caro.js
@@ -4,42 +4,68 @@ import img4 from "./l4-min.jpg";
 import img5 from "./l5-min.jpg";
 import caro from "./caro.svg";
 import "./newlanding.css";
+import Carousel from "react-bootstrap/Carousel";
+
 export const Caro = () => {
+  const [index, setIndex] = React.useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  // return (
+  //   <div
+  //     className="caro-container caro"
+  //     style={{
+  //       display: "flex",
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //     }}
+  //   >
+  //     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  //       <ol class="carousel-indicators">
+  //         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+  //         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+  //         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  //       </ol>
+  //       <div className="" style={{ borderRadius: "30px", width: "100%" }} class="carousel-inner">
+  //         <div class="carousel-item active">
+  //           <img class="" src={caro} alt="First slide" />
+  //         </div>
+  //         <div class="carousel-item">
+  //           <img class="" src={caro} alt="Second slide" />
+  //         </div>
+  //         <div class="carousel-item">
+  //           <img class="" src={caro} alt="Third slide" />
+  //         </div>
+  //       </div>
+  //       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  //         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  //         <span class="sr-only">Previous</span>
+  //       </a>
+  //       <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  //         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  //         <span class="sr-only">Next</span>
+  //       </a>
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div
-      className="caro-container caro"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <Carousel
+      style={{  width: "100%" }}
+      activeIndex={index}
+      onSelect={handleSelect}
     >
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div className="" style={{ borderRadius: "30px" }} class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="" src={caro} alt="First slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="" src={caro} alt="Second slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="" src={caro} alt="Third slide" />
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
+      <Carousel.Item>
+        <img style={{ borderRadius: "60px", padding: "50px " }} className=" w-100" src={caro} />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img style={{ borderRadius: "60px", padding: "50px" }} className=" w-100" src={caro} />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img style={{ borderRadius: "60px", padding: "50px" }} className=" w-100" src={caro} />
+      </Carousel.Item>
+    </Carousel>
   );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,86 +2,96 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import Sidebar from "../sidebar/Sidebar";
+import HomeIcon from "@mui/icons-material/Home";
+import navlogo from "./navlogo.svg";
 
 export const Navbar = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	return (
-		<div className="navcontainer">
-			<div className="navinnercontainer">
-				<div
-					className="navlogocontainer"
-					onClick={() => {
-						navigate("/");
-					}}
-				>
-					<span>Tina Rosario</span>
-				</div>
-				<div className="navoptions">
-					<span
-						className="navlink1"
-						onClick={() => {
-							navigate("/");
-						}}
-					>
-						Gallery
-					</span>
-					<span
-						className="navlink2"
-						onClick={async () => {
-							await navigate("/");
-							// document.getElementById("contactusdiv").scrollIntoView();
-							document.getElementById("testimonialsdiv").scrollIntoView({
-								behavior: "smooth"
-							});
-						}}
-					>
-						Testimonials
-					</span>
-					<span
-						className="navlink3"
-						onClick={() => {
-							navigate("/about");
-						}}
-					>
-						About Us
-					</span>
-					<span
-						className="navlink5"
-						onClick={() => {
-							navigate("/fabrics");
-						}}
-					>
-						Fabrics
-					</span>
-					<span
-						onClick={async () => {
-							await navigate("/contact");
-							// document.getElementById("contactusdiv").scrollIntoView();
-							document.getElementById("contactusdiv").scrollIntoView({
-								behavior: "smooth"
-							});
-						}}
-						className="navlink4"
-					>
-						Contact Us
-					</span>
-				</div>
-				<div className="navicons">
-					<i class="fa-solid fa-magnifying-glass"></i>
-					<i class="fa-solid fa-cart-shopping"></i>
-					<i class="fa-solid fa-user"></i>
-				</div>
-				<div
-					style={{
-						"@media (min-width: 800px)": {
-							display: "none"
-						}
-					}}
-				>
-					<Sidebar />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="navcontainer">
+      <div className="navinnercontainer">
+        <div
+          className="navlogocontainer"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <img style={{ width: "200px" }} src={navlogo} />
+        </div>
+        <div className="navoptions">
+          <span
+            className="navlink1"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <HomeIcon />
+          </span>
+          <span
+            className="navlink1"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Gallery
+          </span>
+          <span
+            className="navlink2"
+            onClick={async () => {
+              await navigate("/");
+              // document.getElementById("contactusdiv").scrollIntoView();
+              document.getElementById("testimonialsdiv").scrollIntoView({
+                behavior: "smooth",
+              });
+            }}
+          >
+            Testimonials
+          </span>
+          <span
+            className="navlink3"
+            onClick={() => {
+              navigate("/about");
+            }}
+          >
+            About Us
+          </span>
+          <span
+            className="navlink5"
+            onClick={() => {
+              navigate("/fabrics");
+            }}
+          >
+            Fabrics
+          </span>
+          <span
+            onClick={async () => {
+              await navigate("/contact");
+              // document.getElementById("contactusdiv").scrollIntoView();
+              document.getElementById("contactusdiv").scrollIntoView({
+                behavior: "smooth",
+              });
+            }}
+            className="navlink4"
+          >
+            Contact Us
+          </span>
+        </div>
+        <div className="navicons">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <i class="fa-solid fa-cart-shopping"></i>
+          <i class="fa-solid fa-user"></i>
+        </div>
+        <div
+          style={{
+            "@media (min-width: 800px)": {
+              display: "none",
+            },
+          }}
+        >
+          <Sidebar />
+        </div>
+      </div>
+    </div>
+  );
 };
