a265d063c80646f31d34554ff5b0c39c3d26ab55
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-11-13 00:31:30 +0530

    done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,121 +7,127 @@ import navlogo from "./navlogo.svg";
 import Select from "react-select";
 
 export const Navbar = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const searchoptions = [
-    { value: "chocolate", label: "Kurta" },
-    { value: "strawberry", label: "Saree" },
-    { value: "vanilla", label: "Lehenga" },
-    { value: "vanilla", label: "Dress" },
-    { value: "vanilla", label: "Handloom dress" },
-    { value: "vanilla", label: "Pant" },
-    { value: "vanilla", label: "Shirt" },
-    { value: "vanilla", label: "Trouser" },
-    { value: "vanilla", label: "Blouse,Designer blouses" },
-    { value: "vanilla", label: "Aari work" },
-    { value: "vanilla", label: "Dresses" },
-    { value: "vanilla", label: "Accessories" },
-    { value: "vanilla", label: "Blazer" },
-    { value: "vanilla", label: "Customised clothes" },
-    { value: "vanilla", label: "Ready made garments" },
-    { value: "vanilla", label: "Chanderi" },
-    { value: "vanilla", label: "Cotton" },
-    { value: "vanilla", label: "Umberalla cut" },
-    { value: "vanilla", label: "Bridegroom" },
-    { value: "vanilla", label: "Wedding" },
-  ];
+	const searchoptions = [
+		{ value: "chocolate", label: "Kurta" },
+		{ value: "strawberry", label: "Saree" },
+		{ value: "vanilla", label: "Lehenga" },
+		{ value: "vanilla", label: "Dress" },
+		{ value: "vanilla", label: "Handloom dress" },
+		{ value: "vanilla", label: "Pant" },
+		{ value: "vanilla", label: "Shirt" },
+		{ value: "vanilla", label: "Trouser" },
+		{ value: "vanilla", label: "Blouse,Designer blouses" },
+		{ value: "vanilla", label: "Aari work" },
+		{ value: "vanilla", label: "Dresses" },
+		{ value: "vanilla", label: "Accessories" },
+		{ value: "vanilla", label: "Blazer" },
+		{ value: "vanilla", label: "Customised clothes" },
+		{ value: "vanilla", label: "Ready made garments" },
+		{ value: "vanilla", label: "Chanderi" },
+		{ value: "vanilla", label: "Cotton" },
+		{ value: "vanilla", label: "Umberalla cut" },
+		{ value: "vanilla", label: "Bridegroom" },
+		{ value: "vanilla", label: "Wedding" }
+	];
 
-  return (
-    <div className="navcontainer">
-      <div className="navinnercontainer">
-        <div
-          className="navlogocontainer"
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          <img style={{ width: "300px", padding: "10px", cursor: "pointer" }} src={navlogo} />
-        </div>
-        <div className="navoptions">
-          <span
-            className="navlink1"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            <HomeIcon />
-          </span>
-          <span
-            className="navlink3"
-            onClick={() => {
-              navigate("/about");
-            }}
-          >
-            About Us
-          </span>
-          <span
-            className="navlink1"
-            onClick={() => {
-              navigate("/gallerycaro");
-            }}
-          >
-            Gallery
-          </span>
-          <span
-            className="navlink5"
-            onClick={async () => {
-              await navigate("/");
-              // document.getElementById("contactusdiv").scrollIntoView();
-              document.getElementById("services").scrollIntoView({
-                behavior: "smooth",
-              });
-            }}
-          >
-            Services
-          </span>
-          <span
-            className="navlink5"
-            onClick={async () => {
-              await navigate("/");
-              // document.getElementById("contactusdiv").scrollIntoView();
-              document.getElementById("products").scrollIntoView({
-                behavior: "smooth",
-              });
-            }}
-          >
-            Products
-          </span>
-          <span
-            className="navlink2"
-            onClick={async () => {
-              await navigate("/");
-              // document.getElementById("contactusdiv").scrollIntoView();
-              document.getElementById("Landingtesti").scrollIntoView({
-                behavior: "smooth",
-              });
-            }}
-          >
-            Testimonials
-          </span>
-          <span
-            onClick={async () => {
-              await navigate("/contact");
-              // document.getElementById("contactusdiv").scrollIntoView();
-              document.getElementById("contactusdiv").scrollIntoView({
-                behavior: "smooth",
-              });
-            }}
-            className="navlink4"
-          >
-            Contact Us
-          </span>
-          {/* <span className="navlink5">Testimonials</span> */}
-        </div>
-        <div className="navicons">
-          <i class="fa-solid fa-magnifying-glass" style={{ marginRight: "10px" }}></i>
-          <Select options={searchoptions}></Select>
-          {/* <input
+	return (
+		<div className="navcontainer">
+			<div className="navinnercontainer">
+				<div
+					className="navlogocontainer"
+					onClick={() => {
+						navigate("/");
+					}}
+				>
+					<img
+						style={{ width: "300px", padding: "10px", cursor: "pointer" }}
+						src={navlogo}
+					/>
+				</div>
+				<div className="navoptions">
+					<span
+						className="navlink1"
+						onClick={() => {
+							navigate("/");
+						}}
+					>
+						<HomeIcon />
+					</span>
+					<span
+						className="navlink3"
+						onClick={() => {
+							navigate("/about");
+						}}
+					>
+						About Us
+					</span>
+					<span
+						className="navlink1"
+						onClick={() => {
+							navigate("/gallerycaro");
+						}}
+					>
+						Gallery
+					</span>
+					<span
+						className="navlink5"
+						onClick={async () => {
+							await navigate("/");
+							// document.getElementById("contactusdiv").scrollIntoView();
+							document.getElementById("services").scrollIntoView({
+								behavior: "smooth"
+							});
+						}}
+					>
+						Services
+					</span>
+					<span
+						className="navlink5"
+						onClick={async () => {
+							await navigate("/");
+							// document.getElementById("contactusdiv").scrollIntoView();
+							document.getElementById("products").scrollIntoView({
+								behavior: "smooth"
+							});
+						}}
+					>
+						Products
+					</span>
+					<span
+						className="navlink2"
+						onClick={async () => {
+							await navigate("/");
+							// document.getElementById("contactusdiv").scrollIntoView();
+							document.getElementById("Landingtesti").scrollIntoView({
+								behavior: "smooth"
+							});
+						}}
+					>
+						Testimonials
+					</span>
+					<span
+						onClick={async () => {
+							await navigate("/contact");
+							// document.getElementById("contactusdiv").scrollIntoView();
+							document.getElementById("contactusdiv").scrollIntoView({
+								behavior: "smooth"
+							});
+						}}
+						className="navlink4"
+					>
+						Contact Us
+					</span>
+					{/* <span className="navlink5">Testimonials</span> */}
+				</div>
+				<div className="navicons">
+					<i
+						class="fa-solid fa-magnifying-glass"
+						style={{ marginRight: "10px" }}
+					></i>
+					<Select options={searchoptions}></Select>
+					{/* <input
 						type="text"
 						style={{
 							width: "500px",
@@ -130,16 +136,16 @@ export const Navbar = () => {
 							border: "1px solid black"
 						}}
 					/> */}
-          {/* <select>
+					{/* <select>
 						<option value="A">Apple</option>
 						<option value="B">Banana</option>
 						<option value="C">Cranberry</option>
 					</select> */}
-          {/* <i class="fa-solid fa-cart-shopping"></i> */}
-          {/* <i class="fa-solid fa-user"></i> */}
-        </div>
+					{/* <i class="fa-solid fa-cart-shopping"></i> */}
+					{/* <i class="fa-solid fa-user"></i> */}
+				</div>
 
-        {/* <div
+				{/* <div
 					style={{
 						"@media (min-width: 800px)": {
 							display: "none"
@@ -148,7 +154,7 @@ export const Navbar = () => {
 				>
 					<Sidebar />
 				</div> */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
