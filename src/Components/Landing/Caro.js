bcbad9af2114c4ff23deb1c11ead801e7fc550f3
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-11-13 18:39:51 +0530

    gallery

--- src/Components/Landing/Caro.js
+++ src/Components/Landing/Caro.js
@@ -5,158 +5,207 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Caro = () => {
-  const navigate = useNavigate();
-  return (
-    <Carousel
-      style={{ height: "650px" }}
-      dynamicHeight={true}
-      infiniteLoop
-      useKeyboardArrows
-      autoPlay
-      showThumbs={false}
-      showStatus={false}
-      showIndicators={false}
-    >
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/875%20(1).png?alt=media&token=35b5cb14-4625-4bf6-b6fb-10fb10024bf1"
-          }
-          height={"650px"}
-        />
-      </div>
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/880.png?alt=media&token=a852ba99-208a-41be-a685-cc9c0714aa32"
-          }
-          height={"650px"}
-        />
-      </div>
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/876.png?alt=media&token=eae68b41-d032-4ee7-9c00-887932c7ce23"
-          }
-          height={"650px"}
-        />
-      </div>
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/879.png?alt=media&token=02658554-1e8b-4531-a93d-809a059f15d2"
-          }
-          height={"650px"}
-        />
-      </div>
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/881.png?alt=media&token=18f30af8-f282-4413-bcb5-c51f59cdee75"
-          }
-          height={"650px"}
-        />
-      </div>
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/878.png?alt=media&token=94c867a3-6eae-465f-b94f-1f64ba14af90"
-          }
-          height={"650px"}
-        />
-      </div>
-      <div style={{ padding: "15px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div
-          onClick={() => navigate("/contact")}
-          style={{
-            backgroundColor: "transparent",
-            width: "350px",
-            height: "300px",
-            marginTop: "100px",
-            position: "absolute",
-            cursor: "pointer",
-          }}
-        ></div>
-        <img
-          style={{ borderRadius: "30px" }}
-          src={
-            "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/877.png?alt=media&token=f668b848-e98e-4b9f-9bd0-beb9f0f68377"
-          }
-          height={"650px"}
-        />
-      </div>
-    </Carousel>
-  );
+	const navigate = useNavigate();
+	return (
+		<Carousel
+			style={{ height: "650px" }}
+			dynamicHeight={true}
+			infiniteLoop
+			useKeyboardArrows
+			autoPlay
+			showThumbs={false}
+			showStatus={false}
+			showIndicators={false}
+		>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/Jodha_Caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/875%20(1).png?alt=media&token=35b5cb14-4625-4bf6-b6fb-10fb10024bf1"
+					}
+					height={"650px"}
+				/>
+			</div>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/Mendesigner_Caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/880.png?alt=media&token=a852ba99-208a-41be-a685-cc9c0714aa32"
+					}
+					height={"650px"}
+				/>
+			</div>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/Deshna_caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/876.png?alt=media&token=eae68b41-d032-4ee7-9c00-887932c7ce23"
+					}
+					height={"650px"}
+				/>
+			</div>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/handembroidery_caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/879.png?alt=media&token=02658554-1e8b-4531-a93d-809a059f15d2"
+					}
+					height={"650px"}
+				/>
+			</div>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/organic_wear_caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/881.png?alt=media&token=18f30af8-f282-4413-bcb5-c51f59cdee75"
+					}
+					height={"650px"}
+				/>
+			</div>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/laskshna_caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/878.png?alt=media&token=94c867a3-6eae-465f-b94f-1f64ba14af90"
+					}
+					height={"650px"}
+				/>
+			</div>
+			<div
+				style={{
+					padding: "15px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<div
+					onClick={() => navigate("/Productmsg/Rambai_caro")}
+					style={{
+						backgroundColor: "transparent",
+						width: "350px",
+						height: "300px",
+						marginTop: "100px",
+						position: "absolute",
+						cursor: "pointer"
+					}}
+				></div>
+				<img
+					style={{ borderRadius: "30px" }}
+					src={
+						"https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/877.png?alt=media&token=f668b848-e98e-4b9f-9bd0-beb9f0f68377"
+					}
+					height={"650px"}
+				/>
+			</div>
+		</Carousel>
+	);
 };
--- src/Components/Pages/GalleryCaro.js
+++ src/Components/Pages/GalleryCaro.js
@@ -5,87 +5,119 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import MoonLoader from "react-spinners/ClipLoader";
 
 const GalleryCaro = () => {
-  const [loading, setLoading] = useState(true);
-  const [pagedata, setPagedata] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [pagedata, setPagedata] = useState([]);
 
-  const getdata = async () => {
-    const data = await fetch("http://products.tinarosario.com/api/Gallery-images")
-      .then((res) => res.json())
-      .then((result) => {
-        // console.log(result.data[0].attributes.galleryimages.gallery);
-        const temp = result.data[0].attributes.galleryimages.gallery;
-        setPagedata(temp);
-        setLoading(false);
-      });
-  };
-  console.log(pagedata);
+	const getdata = async () => {
+		const data = await fetch(
+			"http://products.tinarosario.com/api/Gallery-images"
+		)
+			.then((res) => res.json())
+			.then((result) => {
+				// console.log(result.data[0].attributes.galleryimages.gallery);
+				const temp = result.data[0].attributes.galleryimages.gallery;
+				setPagedata(temp);
+				setLoading(false);
+			});
+	};
+	console.log(pagedata);
 
-  useEffect(() => {
-    getdata();
-  }, []);
+	useEffect(() => {
+		getdata();
+	}, []);
 
-  return (
-    <div className="galleryCont">
-      {loading ? (
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-          }}
-        >
-          <MoonLoader color="#4C2A76" size={50} />
-        </div>
-      ) : (
-        <div className="galleryCont">
-          <h2 className="gallerytitle">By the House</h2>
-          <div
-            id="carouselExampleIndicators"
-            class="carousel slide"
-            data-ride="carousel"
-            style={{ marginBottom: "80px", marginTop: "80px" }}
-          >
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div className="innercarodiv">
-                  {" "}
-                  <img className="caroImg" src={pagedata[0]} alt="First slide" />
-                </div>
-              </div>
-              {pagedata.slice(1, pagedata.length).map((img) => {
-                return (
-                  <div class="carousel-item">
-                    <div className="innercarodiv">
-                      <img className="caroImg" src={img} alt="Second slide" />
-                    </div>
-                  </div>
-                );
-              })}
-              {/* <div class="carousel-item">
+	return (
+		<div className="galleryCont">
+			{loading ? (
+				<div
+					style={{
+						display: "flex",
+						width: "100%",
+						alignItems: "center",
+						justifyContent: "center",
+						height: "100vh"
+					}}
+				>
+					<MoonLoader color="#4C2A76" size={50} />
+				</div>
+			) : (
+				<div className="galleryCont">
+					<h2 className="gallerytitle">By the House</h2>
+					<div
+						id="carouselExampleIndicators"
+						class="carousel slide"
+						data-ride="carousel"
+						style={{ marginBottom: "80px", marginTop: "40px" }}
+					>
+						<ol class="carousel-indicators">
+							<li
+								data-target="#carouselExampleIndicators"
+								data-slide-to="0"
+								class="active"
+							></li>
+							<li
+								data-target="#carouselExampleIndicators"
+								data-slide-to="1"
+							></li>
+							<li
+								data-target="#carouselExampleIndicators"
+								data-slide-to="2"
+							></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<div className="innercarodiv">
+									{" "}
+									<img
+										className="caroImg"
+										src={pagedata[0]}
+										alt="First slide"
+									/>
+								</div>
+							</div>
+							{pagedata.slice(1, pagedata.length).map((img) => {
+								return (
+									<div class="carousel-item">
+										<div className="innercarodiv">
+											<img className="caroImg" src={img} alt="Second slide" />
+										</div>
+									</div>
+								);
+							})}
+							{/* <div class="carousel-item">
 								<img class="d-block w-100" src="..." alt="Second slide" />
 							</div>
 							<div class="carousel-item">
 								<img class="d-block w-100" src="..." alt="Third slide" />
 							</div> */}
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+						</div>
+						<a
+							class="carousel-control-prev"
+							href="#carouselExampleIndicators"
+							role="button"
+							data-slide="prev"
+						>
+							<span
+								class="carousel-control-prev-icon"
+								aria-hidden="true"
+							></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a
+							class="carousel-control-next"
+							href="#carouselExampleIndicators"
+							role="button"
+							data-slide="next"
+						>
+							<span
+								class="carousel-control-next-icon"
+								aria-hidden="true"
+							></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
 
-          {/* <div className="galleryCaro" style={{ marginBottom: "80px" }}>
+					{/* <div className="galleryCaro" style={{ marginBottom: "80px" }}>
 						<Carousel
 							infiniteLoop
 							useKeyboardArrows
@@ -103,10 +135,10 @@ const GalleryCaro = () => {
 							})}
 						</Carousel>
 					</div> */}
-        </div>
-      )}
-    </div>
-  );
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default GalleryCaro;
