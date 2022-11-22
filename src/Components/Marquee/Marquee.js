c063688a875ba3b7f37257bfa63b59b3b5a3dc62
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-11-23 00:07:23 +0530

    marque

--- src/Components/Marquee/Marquee.js
+++ src/Components/Marquee/Marquee.js
@@ -2,23 +2,24 @@ import React from "react";
 import Marquee from "react-fast-marquee";
 
 export const Marqueeline = () => {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "30px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#9ADCE1",
-      }}
-    >
-      <Marquee gradient={false} style={{ color: "black" }} speed={80}>
-        Welcome! Third party payment integration will be launched shortly. Meanwhile explore our site & Get 10% Off on
-        first order
-        <div style={{ width: "400px" }}></div>
-      </Marquee>
-      {/* <h1>Hello</h1> */}
-    </div>
-  );
+	return (
+		<div
+			style={{
+				width: "100%",
+				height: "30px",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				backgroundColor: "#9ADCE1"
+			}}
+		>
+			<Marquee gradient={false} style={{ color: "black" }} speed={80}>
+				Welcome! Third-party payment options will be launched shortly.
+				Meanwhile, explore our site and contact us if you like an outfit. Avail
+				10% Off on your first order
+				<div style={{ width: "400px" }}></div>
+			</Marquee>
+			{/* <h1>Hello</h1> */}
+		</div>
+	);
 };
