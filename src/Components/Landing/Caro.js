0c1928fc088959ff8db16ebb5293bcf8edb9089c
jeremyasirwaad <71336703+jeremyasirwaad@users.noreply.github.com>
2023-02-01 01:24:36 +0530

    Update Caro.js
    asd

--- src/Components/Landing/Caro.js
+++ src/Components/Landing/Caro.js
@@ -256,8 +256,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F1.jpg?alt=media&token=01ed7cba-b7c9-4a99-8e1e-a85ac12a97c3"
-                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F1.jpg?alt=media&token=d5de8222-d361-40c0-8847-6eb0b50d39cb"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F1.jpg?alt=media&token=057c0bb3-8b7e-4a33-8876-e598e33fd955"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F1.jpg?alt=media&token=038ed0c5-80e5-4798-81e7-1fc44fa97eb5"
               }
             />
           </div>
@@ -282,8 +282,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? men
-                  : "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/880.png?alt=media&token=a852ba99-208a-41be-a685-cc9c0714aa32"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F2.jpg?alt=media&token=e0dd56c8-af05-45a8-83a1-7c5fad5f38ff"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F3.jpg?alt=media&token=a2170558-70cc-4b0c-a4a3-4f6b38a9c25c"
               }
             />
           </div>
@@ -308,8 +308,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? des
-                  : "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/Group%20812.png?alt=media&token=efd781ba-1762-45ce-98d7-faeba727455a"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F4.jpg?alt=media&token=a3bfa5e3-109f-471e-bb39-70ebfd6cadb0"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F4.jpg?alt=media&token=8aa4a7a9-ced7-4c98-8239-d122407e550b"
               }
             />
           </div>
@@ -335,8 +335,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? hand
-                  : "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/Group%20811.png?alt=media&token=c5c28e92-9f2f-4e37-ad2c-bf8c82d6651d"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F7.jpg?alt=media&token=850a6e65-7bcb-468b-a098-56dddb578355"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F5.jpg?alt=media&token=f60b5e24-9eb4-414c-896e-ce5cdb0b698d"
               }
             />
           </div>
@@ -362,8 +362,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? org
-                  : "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/881.png?alt=media&token=18f30af8-f282-4413-bcb5-c51f59cdee75"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F5.jpg?alt=media&token=e52e3777-28de-4475-86db-04c8a8adf2cc"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F6.jpg?alt=media&token=cfffdedb-5017-418f-bf47-e8996d06c1d6"
               }
             />
           </div>
@@ -389,8 +389,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? lak
-                  : "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/878.png?alt=media&token=94c867a3-6eae-465f-b94f-1f64ba14af90"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F8.jpg?alt=media&token=54160b1c-fa3a-4197-ad4c-a290b43ada8b"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F7.jpg?alt=media&token=f0aa1fdf-1dd7-4dce-9191-d0a39e7ee3fb"
               }
             />
           </div>
@@ -431,8 +431,8 @@ export const Caro = () => {
               }}
               src={
                 window.innerWidth <= 700
-                  ? rambai
-                  : "https://firebasestorage.googleapis.com/v0/b/teenz-fe41d.appspot.com/o/Group%20808.png?alt=media&token=239ff70b-b01d-4bef-bf80-c59cf6faf343"
+                  ? "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20MOBILE%2F6.jpg?alt=media&token=11efa811-26e6-4323-bbe1-8f794830872c"
+                  : "https://firebasestorage.googleapis.com/v0/b/tinarosario-41753.appspot.com/o/CAROUSEL%20-%20WEBSITE%2F8.jpg?alt=media&token=0e3b1d1f-26d3-4645-8651-17d6f5efa360"
               }
             />
           </div>
